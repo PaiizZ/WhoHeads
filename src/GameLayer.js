0d4821d701c636ba164f1753f3121847985df673
PaiizZ <paiizzmk@gmail.com>
2015-03-23 22:30:59 +0700

    Picture can move up and down

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -6,31 +6,42 @@ var GameLayer = cc.LayerColor.extend({
     this.addChild( this.bg );
 
     this.whoGun = new whoGun();
-    this.whoGun.setPosition( new cc.Point( screenWidth/2 , screenHeight/3.5  ) );
+    this.whoGun.setPosition( new cc.Point( screenWidth/2 , 0  ) );
     this.addChild( this.whoGun );
+    
 
-
- this.hummerBlue = new hummerBlue();
+    this.hummerBlue = new hummerBlue();
     this.hummerBlue.setPosition( new cc.Point( screenWidth-150 , screenHeight/1.5  ) );
     this.addChild( this.hummerBlue );
 
-this.hummerBlue2 = new hummerBlue2();
+    this.hummerBlue2 = new hummerBlue2();
     this.hummerBlue2.setPosition( new cc.Point( screenWidth-250 , screenHeight/1.8  ) );
     this.addChild( this.hummerBlue2 );
 
+    this.addKeyboardHandlers();
+this.whoGun.scheduleUpdate();
     return true;
+  },
+  onKeyUp: function( e ) {
+    if ( e == 40 ) {
+      this.whoGun.hitGun();
+    }
+    if ( e == 32 ) {
+      this.whoGun.hitGun();
+    }
+  },
+
+  addKeyboardHandlers: function() {
+    var self = this;
+    cc.eventManager.addListener({
+      event: cc.EventListener.KEYBOARD,
+      onKeyPressed : function( e ) {
+        self.onKeyUp( e );
+      },
+    }, this);
   }
 
-
-
-
-
-
 });
-
-
-
-
 
 
 var StartScene = cc.Scene.extend({
--- src/whoGun.js
+++ src/whoGun.js
@@ -1,8 +1,27 @@
 var whoGun = cc.Sprite.extend({
-    ctor: function() {
-        this._super();
-        this.initWithFile( 'res/images/whoGun.png' );
-
-    },
+	ctor: function() {
+		this._super();
+		this.initWithFile( 'res/images/whoGun.png' );
+		this.direction = 0 ;
+	},
+	update: function( dt ) {
+		if (this.direction != whoGun.DIR.Hit) {
+			if(this.getPositionY() == 180) {
+				this.setPositionY( this.getPositionY()  );
+			}
+			else
+				this.setPositionY( this.getPositionY() + 2 );
+		}
+		else
+			this.setPositionY( this.getPositionY() - 2 );
+	},
+	hitGun: function() {
+		this.direction = whoGun.DIR.Hit ;
+		
+	},
 
 });
+
+whoGun.DIR = {
+	Hit: 1
+};
